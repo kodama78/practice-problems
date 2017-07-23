577040d10e8d13aa775ca7eb131fb02be0164737
Daniel <daniel.paschal@learningfuze.com>
2017-07-23 12:02:27 -0700

    added dependency source js file

---
+++ practice35/dependencies.js
@@ -0,0 +1,34 @@
+
+
+var dependencies = [
+  {
+    name: 'superCool',
+    dependencies: [
+      'awesome',
+      'stdio',
+      'moocow'
+    ]
+  },
+  {
+    name: 'mrMan',
+    dependencies: [
+      'sayWhat',
+      'stdio',
+      'iPity'
+    ]
+  },
+  {
+    name: 'moocow',
+    dependencies: [
+      'bovine',
+      'quadrapated'
+    ]
+  },
+  {
+    name: 'stdio',
+    dependencies: [
+      'c_lib',
+      'keys',
+    ]
+  }
+]
