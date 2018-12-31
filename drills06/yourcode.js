caeeeaf87bcd405aaac7986b2dabb742ece6dcfb
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-12-30 22:22:02 -0800

    removed test code

--- drills06/yourcode.js
+++ drills06/yourcode.js
@@ -1,35 +1,23 @@
 
 
-function removeClassFromElement(target, classToRemove){
-	$(target).removeClass( classToRemove );
+function removeClassFromElement(){
+
 }
 
-function toggleClassOnElement(target, classToToggle){
-	$(target).toggleClass( classToToggle );
+function toggleClassOnElement(){
+	
 }
 
-function hideElements( target, removalType ){
-	switch( removalType ){
-		case 'hide':
-			$(target).hide();
-			break;
-		case 'removeChildren':
-			$(target).empty();
-			break;
-		case 'removeSelf':
-			$(target).remove();
-			break;
-	}
+function hideElements(  ){
+	
 }
 
-function addAttributeToElement( target, attributeKey, attributeValue){
-	$(target).attr(attributeKey, attributeValue);
+function addAttributeToElement( ){
+	
 }
 
-function putPosInElement( element ){
-	const position = $(element).position();
-	$(element).html( `x: ${position.left}px<br>y: ${position.top}px`);
-	return {x: position.left, y: position.top}
+function putPosInElement(  ){
+	
 }
 
 
