8a0d2d965a4bb566e3eaec4f354c0989701e9c3b
Joshua Ross <jrossdiego@gmail.com>
2019-07-08 16:06:22 -0700

    made seperate variables inside function

--- practice29/main.js
+++ practice29/main.js
@@ -3,10 +3,10 @@ function do_math( num1, num2, operator) {
     return num1 + num2;
   } else if (operator === '-') {
     return num1 - num2;
-  } else if (operator === '*' || 'x' || 'X') {
+  } else if (operator === '*' || operator === 'x' || operator === 'X') {
     return num1 * num2;
   } else if (operator === '/') {
     return num1 / num2;
   }
 }
-console.log("do_math ", do_math(5,2,'*'));
+console.log("do_math ", do_math(5,2,'/'));
