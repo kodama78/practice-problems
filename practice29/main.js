59a23086cc21e26f84971021d716c4a3306c22a9
Joshua Ross <jrossdiego@gmail.com>
2019-07-08 15:04:23 -0700

    complted do math

---
+++ practice29/main.js
@@ -0,0 +1,12 @@
+function do_math( num1, num2, operator) {
+  if (operator === '+') {
+    return num1 + num2;
+  } else if (operator === '-') {
+    return num1 - num2;
+  } else if (operator === '*' || 'x' || 'X') {
+    return num1 * num2;
+  } else if (operator === '/') {
+    return num1 / num2;
+  }
+}
+console.log("do_math ", do_math(5,2,'*'));
