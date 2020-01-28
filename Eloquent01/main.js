2c5177c67d3c908c58d7df34250b28905af32a9e
Joshua Ross <jjross.dev@gmail.com>
2020-01-27 18:20:42 -0800

    completed triangle loop practice problem

--- Eloquent01/main.js
+++ Eloquent01/main.js
@@ -2,7 +2,12 @@ function triangle(char, size) {
   let row = char;
   for (count = 0; count < size; count++) {
     console.log(row);
-    row += char
+    row += char;
   }
 }
 triangle('#', 7);
+
+/* book solution:
+for (let line = "#"; line.length < 8; line += "#")
+  console.log(line);
+*/
