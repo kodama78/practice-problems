71edede21cc7a5833fc43a5689de4cb36c22699c
Joshua Ross <jjross.dev@gmail.com>
2020-03-26 16:28:17 -0700

    completed drill 1.2

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,15 +1,24 @@
 
 function sumArray(list){
   let sum = 0;
-	for (i = 0; i <= list.length; i++) {
+	for (let i = 0; i <= list.length; i++) {
     sum += list[i] || 0;
     console.log(sum)
   }
   return sum;
 }
 
-function fitWithinVal(){
-
+function fitWithinVal(list, value){
+  let sum = 0;
+  for (let i = 0; i <= list.length; i++) {
+    sum += list[i] || 0;
+    if (sum >= value) {
+      sum -= list[i];
+      list.splice(i);
+    }
+    console.log(sum)
+  }
+  return list;
 }
 
 function getAllNamesShorterThan(){
