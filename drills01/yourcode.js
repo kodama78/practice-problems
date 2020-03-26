b8bcb87f7a4b1a523cc8f5b1e1df20395cbf1464
Joshua Ross <jjross.dev@gmail.com>
2020-03-26 15:52:38 -0700

    completeed practice lfz drill 1.1

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,6 +1,11 @@
 
-function sumArray(){
-	
+function sumArray(list){
+  let sum = 0;
+	for (i = 0; i <= list.length; i++) {
+    sum += list[i] || 0;
+    console.log(sum)
+  }
+  return sum;
 }
 
 function fitWithinVal(){
