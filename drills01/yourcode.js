2bf0edbcb45a85cf29395afa4004d7f4225128ab
Joshua Ross <jjross.dev@gmail.com>
2020-03-28 11:34:50 -0700

    completed test 2.2

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -3,22 +3,26 @@ function sumArray(list){
   let sum = 0;
 	for (let i = 0; i <= list.length; i++) {
     sum += list[i] || 0;
-    console.log(sum)
   }
   return sum;
 }
 
 function fitWithinVal(list, value){
   let sum = 0;
-  for (let i = 0; i <= list.length; i++) {
-    sum += list[i] || 0;
+  let fitList = [];
+  for (let i = 0; i < list.length; i++) {
+    console.log(`list number: ${list[i]}`);
+    sum += list[i];
     if (sum >= value) {
       sum -= list[i];
-      list.splice(i);
+      console.log(`sum: ${sum} skipped: ${list[i]} list: ${list}`);
+    } else {
+      fitList.push(list[i]);
+      console.log(`sum: ${sum}, list${list}`);
     }
-    console.log(sum)
   }
-  return list;
+  console.log('done');
+  return fitList;
 }
 
 function getAllNamesShorterThan(){
