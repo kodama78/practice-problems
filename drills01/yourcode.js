7897b2662ffbe482d3a7214253e328ca0183ae3f
Joshua Ross <jjross.dev@gmail.com>
2020-03-29 20:40:19 -0700

    completed drill 1.4

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -34,32 +34,24 @@ function getAllNamesShorterThan(names, numberOfLetters){
   }
   return shorterNames;
 }
-// [
-// {
-//   "familyName": "McGee",
-//     "givenName": "Chuckles",
-//       "greeting": "Miss.",
-//         "age": 28,
-//           "height": 165,
-//             "colors": {
-//     "eye": "green",
-//       "hair": "green"
-//   },
-//   "home address": {
-//     "streetNumber": "12345",
-//       "streetName": "Any St",
-//         "state": "Mo",
-//           "zip": 63102,
-//             "city": "Kansas City"
-//   }
-// }
-// ]
-//- return "Miss. Chuckles McGee\n12345 Any St\nKansas City, Mo 63102"
-function makeLabel(customer){
-  let shipLabel = "";
-  let lineOne = `${customer[0].greeting} ${customer[0].givenName} `;
-  lineOne += `${customer[0].familyName}`;
-  let lineTwo = "";
-  let lineThree = "";
 
+function makeLabel(customers){
+  let label = [];
+  let lineOne, lineTwo, lineThree;
+  for (let i =0; i < customers.length; i++) {
+    const data = customers[i][0];
+    const home = data['home address'];
+    lineOne = `${data.greeting} ${data.givenName} ${data.familyName}\n`;
+    lineTwo = `${home.streetNumber} ${home.streetName}\n`;
+    lineThree = `${home.city}, ${home.state} ${home.zip}`;
+    label.push(lineOne + lineTwo + lineThree);
+  }
+   console.log(label);
 }
+
+const customers = [
+  [{ 'familyName': 'Paschal', 'givenName': 'Daniel', 'greeting': 'Mr.', 'age': 43, 'height': 185, 'colors': { 'eye': 'blue', 'hair': 'brown' }, 'home address': { 'streetNumber': '9200', 'streetName': 'Irvine Center Drive', 'state': 'CA', 'zip': 92618, 'city': 'Irvine' } }],
+  [{ 'familyName': 'McGee', 'givenName': 'Chuckles', 'greeting': 'Miss.', 'age': 28, 'height': 165, 'colors': { 'eye': 'green', 'hair': 'green' }, 'home address': { 'streetNumber': '12345', 'streetName': 'Any St', 'state': 'Mo', 'zip': 63102, 'city': 'Kansas City' } }],
+];
+
+makeLabel(customers);
